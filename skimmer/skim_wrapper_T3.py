fc39b9fbbeebe71b90750321a7646db453eca603
Andrea Piccinelli <andrea8395@gmail.com>
2025-12-10 04:03:01 -0500

    Cleaning and refining the skimmer scripts

--- skimmer/skim_wrapper_T3.py
+++ skimmer/skim_wrapper_T3.py
@@ -22,9 +22,8 @@ nevents  = args.nevents
 print("Full list of input arguments", args)
 print("Cut argument", args.cut)
 
-indent = " "*4*2
+indent = " "*4
 
-#print("HELLO")
 s = ["Current working directory:"]
 for f in os.listdir('.'):
     s.append(indent + "{}".format(f))
@@ -45,33 +44,20 @@ for inf in infiles:
     print(s)
     subprocess.check_call(cmd_args)
 
-
-#s = "Sleeping..."
-#print(s)
-#time.sleep(10)
-
 to_skim = local_files
 
 cmd_args = ['nano_postproc.py']
-#cmd_args.extend(['-c','{}'.format(skim_cut.replace(">","\>").replace("<", "\<").replace(" ", ""))])
-cmd_args.extend(['-c',"{}".format(skim_cut.replace(" ", ""))])
-#cmd_args.extend(['-I','CMGTools.TTHAnalysis.tools.nanoAOD.ttH_modules','lepJetBTagDeepFlav,{}'.format(module)])
-#cmd_args.extend(['-I','CMGTools.NanoProc.tools.nanoAOD.lepMVA_run3','lepJetBTagDeepFlav','{}'.format(module)])
+cmd_args.extend(['-c','{}'.format(skim_cut.replace(" ", ""))])
 cmd_args.extend(['-I','CMGTools.NanoProc.tools.nanoAOD.lepMVA_run3','{}'.format(module)])
 cmd_args.extend([out_dir])
 cmd_args.extend(to_skim)
 
 if nevents:
-    #cmd_args.extend(['-n','{}'.format(nevents)])
     cmd_args.extend(['-N','{}'.format(nevents)])
 
 s = "Skim command: {}".format(" ".join(cmd_args))
 print(s)
 subprocess.check_call(cmd_args)
-
-#s = "Sleeping..."
-#print(s)
-#time.sleep(10)
  
 s = ["Current working directory:"]
 print(s)
@@ -79,12 +65,9 @@ for f in os.listdir('.'):
     s.append(indent + "{}".format(f))
 print("\n".join(s))
 
-#raise ValueError("I want to crash!")
-
 # Need to merge any skim outputs into a single file that lobster expects
 to_merge = [ x.rsplit("/")[-1].replace(".root","_Skim.root") for x in to_skim ]
 
-#cmd_args = ['python','haddnano.py','output.root']
 cmd_args = ['haddnano.py','output.root']
 cmd_args.extend(to_merge)
 s = "Merge command: {}".format(" ".join(cmd_args))
